498f7b640367064088c74df23b0eceb328826f80
rpoiu053@gmail.com <rpoiu053@gmail.com>
2023-04-14 20:04:44 +0900

    feat : DecimalDiscrimination
    입력받은 정수가 소수인지 아닌지를 판별

---
+++ GitHubHomeWork/DecimalDiscrimination.cs
@@ -0,0 +1,35 @@
+﻿namespace GitHubHomeWork
+{
+    internal class Program
+    {
+        /* static bool IsPrime(int n)
+        {} 
+        */
+        static bool IsPrime(int n)
+        {
+            if (n == 2)
+                return true;
+            else if (n % 2 == 0)
+                return false;
+            for (int i = 3; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("정수를 입력해주세요 : ");
+            int inPutNum = int.Parse(Console.ReadLine());
+
+
+
+            if (IsPrime(inPutNum) == true)
+                Console.WriteLine("{0}은(는) 소수입니다.", inPutNum);
+            else
+                Console.WriteLine("{0}은(는) 소수가 아닙니다.", inPutNum);
+        }
+
+    }
+}
