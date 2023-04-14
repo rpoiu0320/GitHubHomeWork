55d2e81a6edbb14025c595c7335a5ffe077f99c4
rpoiu053@gmail.com <rpoiu053@gmail.com>
2023-04-14 19:59:47 +0900

    feat : WordNumber
    입력받은 문자열의 단어의 개수를 알려줍니다.

---
+++ GitHubHomeWork/WordNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubHomeWork
+{
+    // 입력 : 안녕하세요. 나는 서울에 사는 홍길동입니다.
+    // 출력 : 6
+    internal class WordNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("문자열을 입력해주세요 : ");
+            string writing = Console.ReadLine();
+            int count = 0;
+
+            for (int i = 0; i < writing.Length; i++)
+            {
+                if (writing[i] == ' ')
+                    count++;
+                else if (i == writing.Length - 1 && writing[i] == '.')
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+
+    }
+}
