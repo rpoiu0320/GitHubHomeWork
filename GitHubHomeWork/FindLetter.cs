e819223015f189f1c079b050e5c461a3176c0f3d
rpoiu053@gmail.com <rpoiu053@gmail.com>
2023-04-14 19:50:13 +0900

    feat: FindLetter
    입력받은 문자열과 단어를 비교하여 단어가 있는 index의 위치를 출력

---
+++ GitHubHomeWork/FindLetter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubHomeWork
+{
+    internal class FindLetter
+    {
+        // 입력 : pineapple in yummy
+        // 출력 : 4
+        static void Main(string[] args)
+        {
+            Console.Write("문자열을 입력해주세요. : ");
+            string sentence = Console.ReadLine();
+            Console.Write("찾을 단어를 입력해주세요. : ");
+            string word = Console.ReadLine();
+            int checkWord = 0;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == word[0])
+                {
+                    int stringNum = i;
+                    for (int j = 0; j < word.Length; i++, j++)
+                    {
+                        if (sentence[i] != word[j])
+                        {
+                            checkWord = -1;
+                            break;
+                        }
+
+                        if (j == word.Length - 1)
+                        {
+                            checkWord = 0;
+                            Console.WriteLine(stringNum);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (checkWord == -1)
+                Console.WriteLine(checkWord);
+        }
+
+    }
+}
