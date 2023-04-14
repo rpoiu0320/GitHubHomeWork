e1229852cf8d82c16e3311ea4f7d99e4688ce8f2
Hyeonuk Ryu <rpoiu053@gmail.com>
2023-04-14 21:51:12 +0900

    feat : CommonIndex
    ConnonIndex 재수성, 미완성

--- GitHubHomeWork/CommonIndex.cs
+++ GitHubHomeWork/CommonIndex.cs
@@ -32,25 +32,38 @@ namespace GitHubHomeWork
                 indexLength = arr3.Length;
 
             int[] num1_2 = new int[indexLength];
+            int[] num1_3 = new int[indexLength];
+            int[] num2_1 = new int[indexLength];
             int[] num2_3 = new int[indexLength];
             int[] num3_1 = new int[indexLength];
+            int[] num3_2 = new int[indexLength];
 
-            for (int i = 0; i < arr2.Length; i++)
-            {
-                int j = 0;
-                int value = arr2[i];
-                if (Array.Exists(arr1, OnIndex => OnIndex == value))
-                    num1_2[j] = Array.IndexOf(arr1, arr2[i]);
-            }
-
+            
+            
             return num1_2;
         }
-        static void CommonIndex_()
+
+        public static int[] IndexOf(int[] firstArray, int[] secondArray)
+        {
+            int indexLength = firstArray.Length;
+            int[] index = { };
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                int j = 0;
+                var check = Array.Exists(firstArray, x => x == secondArray[i]);
+                if (check)
+                {
+                    num1_2[j] = Array.IndexOf(firstArray, secondArray[i]);
+                    j++;
+                }
+            }
+            return 
+        }
+        public static void CommonIndex_()
         {
             int[] arr1 = { 1, 5, 5, 10 };
             int[] arr2 = { 3, 4, 5, 5, 10 };
             int[] arr3 = { 5, 5, 10, 20 };
-
 
             int indexLength = arr1.Length;
             if (indexLength < arr2.Length)
@@ -59,12 +72,11 @@ namespace GitHubHomeWork
             if (indexLength < arr3.Length)
                 indexLength = arr3.Length;
 
-            int[] index = FindCommonItems(arr1, arr2, arr3);
+            Array.Resize(ref arr1, indexLength);
+            Array.Resize(ref arr2, indexLength);
+            Array.Resize(ref arr3, indexLength);
 
-            for(int i = 0; i < indexLength; i++)
-            {
-                Console.WriteLine(index[i]);
-            }
+            FindCommonItems(arr1, arr2, arr3);
         }
 
         public static void Main()
